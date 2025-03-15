19a97b9ff010426590fb7b58cff9b38a10939f05
V-Karch <luna.karch05@gmail.com>
2025-03-15 16:43:35 -0400

    Used sample read_csv function provided in tutorial with slight modification to retrieve headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
+
+fn read_csv<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+    let file: File = File::open(filename)?;
+    let mut rdr: csv::Reader<File> = csv::Reader::from_reader(file);
+
+    if let Ok(header) = rdr.headers() {
+        println!("{:?}", header);
+    }
+
+    for result in rdr.records() {
+        let record: csv::StringRecord = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = "sample.csv";
+    read_csv(filename)
 }
