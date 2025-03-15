4c1493fe6e6bf72a4c6cb32f0708b27aa647334d
V-Karch <luna.karch05@gmail.com>
2025-03-15 16:53:54 -0400

    implemented function to read only headers and return them

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,19 @@ use std::error::Error;
 use std::fs::File;
 use std::path::Path;
 
-fn read_csv<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
+fn read_csv_headers<P: AsRef<Path>>(filename: P) -> Result<Vec<String>, Box<dyn Error>> {
     let file: File = File::open(filename)?;
     let mut rdr: csv::Reader<File> = csv::Reader::from_reader(file);
 
-    if let Ok(header) = rdr.headers() {
-        println!("{:?}", header);
-    }
-
-    for result in rdr.records() {
-        let record: csv::StringRecord = result?;
-        println!("{:?}", record);
-    }
-
-    Ok(())
+    let headers = rdr.headers()?.iter().map(|s| s.to_string()).collect();
+    Ok(headers)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = "sample.csv";
-    read_csv(filename)
+    match read_csv_headers(filename) {
+        Ok(headers) => println!("Headers: {:?}", headers),
+        Err(e) => eprintln!("Error reading headers: {}", e),
+    }
+    Ok(())
 }
